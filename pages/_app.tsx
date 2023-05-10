58ec0772df4bbbb7e0982a994bcebcf98a44f4ff
RinYato <chearithorn@gmail.com>
2023-05-10 22:48:23 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,7 +49,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main>
       <Head key={"head"}>
-        <title>RinYato | Portfolio jenkins</title>
+        <title>RinYato | Portfolio</title>
         <meta name="description" content="rinyato portfolio" />
         <meta
           name="content"
