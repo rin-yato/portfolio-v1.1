037ca3cd36e6b9ba4993dbe3add0a6c18f1c631b
RinYato <chearithorn@gmail.com>
2023-05-18 12:37:52 +0700

    Update Experiences.tsx

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -8,10 +8,11 @@ function Experiences() {
           Experiences <span className="text-indigo-600">:</span>
         </h2>
         <div className="flex flex-col gap-4 text-sm text-slate-600 capitalize">
+          
+          <h3>Full Stack Dev</h3>
+          <h3>NFT contract and Minting Site</h3>
+          <h3>React Three Fiber</h3>
           <h3>Building a portfolio</h3>
-          <h3>Creating an E-Commerce</h3>
-          <h3>Building POS system</h3>
-          <h3>Making a tic tac toe game</h3>
         </div>
       </div>
     </div>
