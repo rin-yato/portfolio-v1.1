e626ffef7bb833299616d5e601d12c9dc231260c
RinYato <chearithorn@gmail.com>
2023-05-18 12:58:12 +0700

    Update Technologies.tsx

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -8,16 +8,16 @@ function Technologies() {
           What I work With <span className="text-indigo-600">:</span>
         </h2>
         <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 text-sm text-slate-600 capitalize">
-          <h3>React</h3>
-          <h3>Sanity.io</h3>
-          <h3>Vue</h3>
-          <h3 className=" lg:hidden">Bootstrap</h3>
-          <h3>Laravel</h3>
+          <h3>React Native</h3>
+          <h3>tRPC</h3>
+          <h3>Primsa</h3>
+          <h3 className=" lg:hidden">Docker</h3>
+          <h3>Nest.js</h3>
           <h3>Tailwindcss</h3>
           <h3>Next.js</h3>
-          <h3>Material UI</h3>
+          <h3>Web3</h3>
           <h3>Nuxt.js</h3>
-          <h3>FramerMotion</h3>
+          <h3>ThreeJS</h3>
         </div>
       </div>
     </div>
